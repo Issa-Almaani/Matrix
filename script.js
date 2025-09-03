66f17d4ebf2db24a255c575b968cf04caa8b030c
Issa-Almaani <issaalmaani987@gmail.com>
2025-09-03 21:46:53 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,10 @@
+function calculateLove() {
+    const num1 = parseFloat(document.getElementById('num1').value);
+    const num2 = parseFloat(document.getElementById('num2').value);
+    if (isNaN(num1) || isNaN(num2)) {
+        document.getElementById('result').innerText = "Please enter valid numbers!";
+        return;
+    }
+    const sum = num1 + num2;
+    document.getElementById('result').innerText = `The love score is 💖 ${sum}% 💖`;
+}
